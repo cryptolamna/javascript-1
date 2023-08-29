e2b9bd240f21392878ec19acd8265f9f11903f37
Pavel V <exacum@Pavels-MacBook-Pro.local>
2023-08-29 21:35:56 +0300

    Семнадцатая ступень - DOM

--- app.js
+++ app.js
@@ -1,144 +1,64 @@
 'use strict';
 
-/* EOL для методов
-// const b = 1;
+/*
+const panelText = document.querySelector('.panel').textContent;
 
-// const a = {
-// 	b,
-// 	getB() {
-// 		return this.b;
-// 	}
-// }
-
-// console.log(a.getB());
+document.querySelector('.panel').textContent = 'New text';
+document.querySelector('.input').value = 'Text';
 */
 
-/* Call, aply
-const audi = {
-	make: 'Audi',
-	model: 'A3',
-	year: 2021,
-	damages: [],
-	addDamage(part, rate) {
-		console.log(`Car ${this.make} ${this.model} ${this.year} add damage ${part} with rate ${rate}`);
-		this.damages.push({
-			part,
-			rate
-		})
+// document.querySelector('.button').addEventListener('click', changeClick);
+
+function submitForm() {
+	const input = document.querySelector('.input').value;
+	if (!input) {
+		return;
+	}
+	const obj = {
+		text: input
+	}
+	const str = JSON.stringify(obj);
+	localStorage.setItem('text', str);
+	document.querySelector('.panel').innerText = input;
+	document.querySelector('.input').value = '';
+	// document.querySelector('.notification').style.display = 'block';
+	// document.querySelector('.notification').classList.add('notification_active');
+	// document.querySelector('.notification').classList.remove('notification_hidden');
+	// console.log(document.querySelector('.notification').getAttribute('class'));
+	// document.querySelector('.notification').setAttribute('class', 'notification');
+	// document.querySelector('.notification').setAttribute('key', 1);
+
+
+	console.log(input);
+}
+function inputChange(e) {
+	if (e.code === 'Enter') {
+		console.log('Enter');
+		submitForm();
 	}
 }
 
+// console.log(document.querySelector('.one').innerText);
+// console.log(document.querySelectorAll('.one'));
+// console.log(document.querySelector('#two').innerText);
+// console.log(document.getElementById('two').innerText);
+// console.log(document.querySelector('[user-id="4"]').innerText);
+// const panelText = 'Panel';
+// const newElement = document.createElement('div');
+// newElement.setAttribute('user-id', 1);
+// newElement.classList.add('panel');
+// newElement.innerText = 'Кнопка';
+// newElement.innerHTML = `<button class="button">${panelText}</button>`;
+// document.querySelector('.test').appendChild(newElement);
 
-const bmw = {
-	make: 'BMW',
-	model: 'X5',
-	year: 2021,
-	damages: [],
-}
+// localStorage.setItem('token1', 'sdasdas');
+// localStorage.setItem('token2', 2);
+// localStorage.setItem('token3', true);
 
-bmw.addDamage = audi.addDamage;
-bmw.addDamage('Капот', 1);
-const addDamageFunc = audi.addDamage;
-addDamageFunc.call(bmw, 'Бампер', 2);
+// console.log(localStorage.getItem('token3'));
+// localStorage.removeItem('token2');
+// localStorage.clear();
 
-addDamageFunc.apply(bmw, ['Бампер', 2]);
-*/
-
-/* Bind
-const audi = {
-	make: 'Audi',
-	model: 'A3',
-	damages: [
-
-	]
-}
-
-const carManipulation = {
-	addDamage(part, rate) {
-		this.damages.push({ part, rate });
-		console.log(`Добавить повреждение на ${this.make} ${this.model}`);
-	}
-}
-
-const addDamageAudi = carManipulation.addDamage.bind(audi);
-addDamageAudi('Wing', 1);
-console.log(audi);
-
-const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Roof');
-addDamageAudiRoof(5);
-addDamageAudiRoof(6);
-
-console.log(audi);
-*/
-
-/* Упражнение - управление this
-function removePassword(reset) {
-	reset ? this.password = undefined : this.password = '1';
-}
-const user = {
-	login: 'a@a.ru',
-	password: '',
-};
-
-const resetPassword = removePassword.bind(user, true);
-resetPassword();
-*/
-
-/* IIFE
-function init() {
-	console.log('Start');
-}
-
-init();
-
-
-(function () {
-	console.log('Start IIFE');
-})();
-*/
-
-/* Замыкания
-function changeBalance() {
-	let balance = 0;
-	let key = 'ad';
-	return function (sum) {
-		balance += sum;
-		console.log(balance);
-	}
-}
-
-const change = changeBalance();
-change(5);
-change(5);
-change(5);
-
-console.dir(change);
-
-const change2 = changeBalance();
-
-console.dir(change2);
-*/
-
-/*Упражнение - работа с замыканиями
-const userInfo = {
-	balance: 0, operations: 0,
-	increase(sum) {
-		this.balance += sum; this.operations++;
-	},
-}
-
-function user() {
-	const userObj = { ...userInfo };
-	return function () { return userObj; }
-};
-
-const user1 = user();
-user1().increase(100);
-
-user1().increase(100);
-console.log(user1());
-
-const user2 = user();
-user2().increase(100);
-console.log(user2());
-*/
+// const obj = JSON.parse('{"a": 1}');
+// console.log(obj);
+// console.log(JSON.stringify(obj));
