91919ca25222c2046633c71457b625425310e545
Pavel V <exacum@Pavels-MacBook-Pro.local>
2023-08-29 14:12:02 +0300

    Шестнадцатая ступень - Управление this

--- app.js
+++ app.js
@@ -1,162 +1,144 @@
 'use strict';
 
+/* EOL для методов
+// const b = 1;
 
-/* Пример scope chain
-const successMessage = 'Успех';
+// const a = {
+// 	b,
+// 	getB() {
+// 		return this.b;
+// 	}
+// }
 
+// console.log(a.getB());
+*/
+
+/* Call, aply
+const audi = {
+	make: 'Audi',
+	model: 'A3',
+	year: 2021,
+	damages: [],
+	addDamage(part, rate) {
+		console.log(`Car ${this.make} ${this.model} ${this.year} add damage ${part} with rate ${rate}`);
+		this.damages.push({
+			part,
+			rate
+		})
+	}
+}
+
+
+const bmw = {
+	make: 'BMW',
+	model: 'X5',
+	year: 2021,
+	damages: [],
+}
+
+bmw.addDamage = audi.addDamage;
+bmw.addDamage('Капот', 1);
+const addDamageFunc = audi.addDamage;
+addDamageFunc.call(bmw, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, ['Бампер', 2]);
+*/
+
+/* Bind
+const audi = {
+	make: 'Audi',
+	model: 'A3',
+	damages: [
+
+	]
+}
+
+const carManipulation = {
+	addDamage(part, rate) {
+		this.damages.push({ part, rate });
+		console.log(`Добавить повреждение на ${this.make} ${this.model}`);
+	}
+}
+
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('Wing', 1);
+console.log(audi);
+
+const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Roof');
+addDamageAudiRoof(5);
+addDamageAudiRoof(6);
+
+console.log(audi);
+*/
+
+/* Упражнение - управление this
+function removePassword(reset) {
+	reset ? this.password = undefined : this.password = '1';
+}
 const user = {
-	name: 'JOHN',
-	roles: []
+	login: 'a@a.ru',
+	password: '',
 };
 
-function addRole(user, role) {
-	if (role === 'admin') {
-		const message = 'Ошибка';
-		console.log(message);
-		return user;
-	}
-	user.roles.push(role);
-	console.log(successMessage);
-
-	function logRoles() {
-		console.log(user.roles);
-	}
-
-	logRoles();
-
-	return user;
-}
-
-console.log(addRole(user, 'dev'));
+const resetPassword = removePassword.bind(user, true);
+resetPassword();
 */
 
-/* Всплытие
-addUser();
-
-console.log(b);
-
-const a = 3;
-var b = 2;
-console.log(b);
-function addUser() {
-	console.log('User added');
-
+/* IIFE
+function init() {
+	console.log('Start');
 }
 
-addUser();
+init();
 
-arr1();
-var arr1 = () => {
-	console.log('arr1');
-}
 
-arr1();
+(function () {
+	console.log('Start IIFE');
+})();
 */
 
-/* this
-console.log(window === this);
-
-function addNum(num1, num2) {
-	console.log(undefined === this); // this === window in no strict
-	return num1 + num2;
-}
-
-const addNum2 = (num1, num2) => {
-	console.log(this === window);
-	return num1 + num2;
-}
-
-const user = {
-	name: 'John',
-	surname: 'Doe',
-	getFullName: function () {
-		console.log(this);
-		return this.name + ' ' + this.surname;
+/* Замыкания
+function changeBalance() {
+	let balance = 0;
+	let key = 'ad';
+	return function (sum) {
+		balance += sum;
+		console.log(balance);
 	}
-};
+}
 
-addNum(1, 2);
-addNum2();
+const change = changeBalance();
+change(5);
+change(5);
+change(5);
 
-user.getFullName();
+console.dir(change);
 
-const user2 = {
-	name: 'J',
-	surname: 'D'
-};
+const change2 = changeBalance();
 
-user2.getFullName = user.getFullName;
-console.log(user2.getFullName());
-
-const getFullName = user2.getFullName;
-console.log(getFullName());
+console.dir(change2);
 */
 
-/* Контекст в методах
-const user = {
-	firstname: 'John',
-	surname: 'Doe',
-	age: 20,
-	getUserInfo() {
-		console.log(this === user);
-		console.log(`${this.firstname} ${this.surname}`);
-
-		const canDrink = () => {
-			console.log(this === undefined);
-			if (this.age >= 18) {
-				console.log('Can drin');
-			} else {
-				console.log('Can"t drink');
-			}
-		}
-		canDrink();
+/*Упражнение - работа с замыканиями
+const userInfo = {
+	balance: 0, operations: 0,
+	increase(sum) {
+		this.balance += sum; this.operations++;
 	},
-	getUserInfoArrow: () => {
-		console.log(this === window && this !== user);
-		console.log(`${this.firstname} ${this.surname}`);
-	}
-}
-user.getUserInfo();
-user.getUserInfoArrow();
-*/
-
-/* Arguments
-function sumNum(num1, num2) {
-	console.log(this === undefined);
-	console.log(arguments);
-	return Array.from(arguments).reduce((acc, value) => acc + value, 0);
 }
 
-const sumNumArr = (num1, num2) => {
-	console.log(this === window);
-	console.log(arguments); // is not defined
-	return Array.from(arguments).reduce((acc, value) => acc + value, 0); // error
-}
-
-
-console.log(sumNumArr(1, 4, 3, 7, 5, 3, 2));
-*/
-
-/* Упражнение - объект в объекте
-*/
-const company = {
-	name: "ООО Агро",
-	employees: [
-		{ name: "Света", getName() { return this.name } },
-	],
-	ceo: { name: "Вася", getName() { return this.name } },
-	getCompanyName() {
-		return this.name;
-	},
-	getCEOName() {
-		return this.ceo.getName();
-	},
-	getEmployeesName() {
-		return this.employees.map(employee => employee.getName());
-	}
+function user() {
+	const userObj = { ...userInfo };
+	return function () { return userObj; }
 };
 
-console.log(company.getCompanyName());
-console.log(company.getCEOName());
-console.log(company.getEmployeesName());
+const user1 = user();
+user1().increase(100);
 
+user1().increase(100);
+console.log(user1());
+
+const user2 = user();
+user2().increase(100);
+console.log(user2());
+*/
